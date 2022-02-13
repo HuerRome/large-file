adb171fc03b3a1b64abe7bca2ba0b536ef4d9b10
HuerRome <4n.rooo@gmail.com>
2022-02-13 11:22:29 -0300

    agregando archivo requirements

--- projecto/settings.py
+++ projecto/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe25c11422da6f33b9354c1963dd2e84f6f1669a98db93d16dfaf71cec83c5d5
-size 3370
+oid sha256:087442021184da1393eb3d467c4749e5d68c9af612d365e20659fa59c133ed3c
+size 3613
